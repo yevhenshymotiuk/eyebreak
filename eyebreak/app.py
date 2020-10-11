45e9a4bbb47f2470f6805caca8aa17a713fee11e
Yevhen Shymotiuk <yevhenshymotiuk@gmail.com>
2020-10-12 00:56:04 +0300

    Add 'Stop' button to stop scheduling

--- eyebreak/app.py
+++ eyebreak/app.py
@@ -10,27 +10,42 @@ class EyeBreakApp(rumps.App):
         self.schedule_button = None
         self.work_time = 30
         self.time_to_next_break = self.work_time
+        self.schedule_button = None
 
         schedule.run_continuously()
 
     @rumps.clicked("Schedule")
     def schedule_break(self, sender):
+        if not self.schedule_button:
+            self.schedule_button = sender
+
+        sender.set_callback(None)
         notify("Break scheduled", title="EyeBreak")
+
+        def update_schedule_button_title():
+            if sender.callback is None and self.time_to_next_break != 0:
+                self.time_to_next_break -= 1
+                sender.title = f"Next break in {self.time_to_next_break} min."
+
+        update_schedule_button_title()
+
         schedule.every(self.work_time).minutes.do(
             self.send_break_notification, sender=sender
         )
-
-        def update_schedule_button_title():
-            self.time_to_next_break -= 1
-            sender.title = f"Next break in {self.time_to_next_break} min."
-
-        update_schedule_button_title()
         schedule.every().minute.do(update_schedule_button_title)
-        sender.set_callback(None)
 
     def send_break_notification(self, sender):
         notify("Take a 10 min. break!", title="EyeBreak", sound="default")
+        self.stop_scheduling(sender)
+
+    def stop_scheduling(self, sender):
         schedule.clear()
-        self.time_to_next_break = self.work_time
-        sender.set_callback(self.schedule_break)
+
         sender.title = "Schedule"
+        sender.set_callback(self.schedule_break)
+
+        self.time_to_next_break = self.work_time
+
+    @rumps.clicked("Stop")
+    def stop(self, _):
+        self.stop_scheduling(self.schedule_button)
