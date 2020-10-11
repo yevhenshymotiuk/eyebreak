3bffde1d0e520d326c31b3ffb1835cd761f1200e
Yevhen Shymotiuk <yevhenshymotiuk@gmail.com>
2020-10-11 20:32:25 +0300

    Disable 'Schedule' button on click and enable on break notification

--- eyebreak/app.py
+++ eyebreak/app.py
@@ -6,19 +6,21 @@ from pync import notify
 class EyeBreakApp(rumps.App):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
         self.scheduled = False
         schedule.run_continuously()
+        self.schedule_button = None
 
     @rumps.clicked("Schedule")
     def schedule_break(self, _):
-        if self.scheduled:
-            rumps.alert("Break is already scheduled")
-        else:
-            notify("Break scheduled", title="EyeBreak")
-            schedule.every(30).minutes.do(self.send_break_notification)
-            self.scheduled = True
+        if not self.schedule_button:
+            self.schedule_button = self.menu["Schedule"]
+
+        notify("Break scheduled", title="EyeBreak")
+        schedule.every(2).seconds.do(self.send_break_notification)
+        self.schedule_button.set_callback(None)
 
     def send_break_notification(self):
         notify("Take a 10 min. break!", title="EyeBreak", sound="default")
         schedule.clear()
-        self.scheduled = False
+        self.schedule_button.set_callback(self.schedule_break)
